43fdd51b4299e8aabe5f6f861c9f7552b56b923a
Joao Vitor Lima Lago Santos <vorque45@gmail.com>
2023-04-06 21:58:09 -0300

    Criação de cliente tls

--- socket-tls/clienteTLS.js
+++ socket-tls/clienteTLS.js
@@ -2,14 +2,22 @@ const tls = require('node:tls');
 const fs = require('node:fs');
 
 const options = {
-    key: fs.readFileSync('private-key.pem'),
-    cert: fs.readFileSync('public-cert.pem'),
-  
-    // This is necessary only if using client certificate authentication.
-    requestCert: false,
-    rejectUnauthorized: false,
+    // Client private key
+    key: fs.readFileSync('certificates/client/private-client-key.pem'),
+
+    // Client public certificate
+    cert: fs.readFileSync('certificates/client/certificate-cert.pem'),
+
+    // This is necessary only if the client uses a self-signed certificate.
+    ca: [ fs.readFileSync('certificates/server/certificate-cert.pem') ]
 };
 
 const cliente = tls.connect(4000, 'localhost', options, () => {
     console.log('Cliente conectado');
+}); 
+
+cliente.setEncoding('utf8');
+
+cliente.on('data', (data) => {
+    console.log(data);
 });
