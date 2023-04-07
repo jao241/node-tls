8f88e6d078d8cc15426b72feb4c615f5248a5db5
Joao Vitor Lima Lago Santos <vorque45@gmail.com>
2023-04-06 23:21:00 -0300

    Envio de imagem

--- socket-tls/servidorTLS.js
+++ socket-tls/servidorTLS.js
@@ -1,30 +1,34 @@
-const tls = require('node:tls');
-const fs = require('node:fs');
+const tls = require("node:tls");
+const fs = require("node:fs");
 
 const options = {
-    // Server private key
-    key: fs.readFileSync('certificates/server/private-server-key.pem'),
+  // Server private key
+  key: fs.readFileSync("certificates/server/private-server-key.pem"),
 
-    // Server public certificate
-    cert: fs.readFileSync('certificates/server/certificate-cert.pem'),
+  // Server public certificate
+  cert: fs.readFileSync("certificates/server/certificate-cert.pem"),
 
-    // This is necessary only if using client certificate authentication.
-    requestCert: true,
+  // This is necessary only if using client certificate authentication.
+  requestCert: true,
 
-    // This is necessary only if the client uses a self-signed certificate.
-    ca: [ fs.readFileSync('certificates/client/certificate-cert.pem') ]
+  // This is necessary only if the client uses a self-signed certificate.
+  ca: [fs.readFileSync("certificates/client/certificate-cert.pem")],
 };
 
 const servidor = tls.createServer(options, (socket) => {
-    const autorizado = socket.authorized;
-    
-    if(autorizado) {
-        socket.write('Olá cliente! Essa mensagem está criptografada!');
-        socket.setEncoding('utf8');
-        socket.pipe(socket);
+  const autorizado = socket.authorized;
 
-        console.log('Mensagem enviada!');
-    }
+  if (autorizado) {
+    socket.write(fs.readFileSync('exemplo.jpeg'));
+    socket.setEncoding("utf8");
+    socket.pipe(socket);
+    
+    console.log("Mensagem enviada!");
+  }
 });
 
-servidor.listen(4000, 'localhost', () => console.log('Servidor iniciado!'));
+servidor.on("message", (data) => {
+  console.log(data);
+});
+
+servidor.listen(4000, "localhost", () => console.log("Servidor iniciado!"));
