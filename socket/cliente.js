175cd8eff424ce8ccbdc995b56bb834f14d1ced0
Joao Vitor Lima Lago Santos <vorque45@gmail.com>
2023-04-06 21:57:48 -0300

    cliente socket

--- socket/cliente.js
+++ socket/cliente.js
@@ -1,29 +1,32 @@
-// const dgram = require('node:dgram');
+const dgram = require('node:dgram');
 
-// const cliente = dgram.createSocket('udp4');
+const cliente = dgram.createSocket('udp4');
 
-// function realizarConexao() {
-//     try {
-//         cliente.connect(4000, 'localhost', () => enviarMensagem('Olá servidor!'));
-//         console.log('Conexão realizada com o servidor!');
+function realizarConexao() {
+    try {
+        cliente.connect(4000, 'localhost', () => enviarMensagem('Olá servidor!'));
+        console.log('Conexão realizada com o servidor!');
         
-//     }catch(error) {
-//         console.warn('Erro ao tentar conectar ao servidor!');
-//         console.error(error);
-//     }
-// }
+    }catch(error) {
+        console.warn('Erro ao tentar conectar ao servidor!');
+        console.error(error);
+    }
+}
 
-// function enviarMensagem(msg) {
-//     try {
-//         cliente.send(msg);
-//     } catch(error) {
-//         console.warn('Erro ao enviar a mensagem!');
-//         console.error(error);
-//         cliente.close();
-//     }
-// }
+function enviarMensagem(msg) {
+    try {
+        cliente.send(msg);
+    } catch(error) {
+        console.warn('Erro ao enviar a mensagem!');
+        console.error(error);
+        cliente.close();
+    }
+}
 
-// realizarConexao();
+realizarConexao();
 
-// cliente.on('message', (data) => console.log(`${data}`));
+cliente.on('message', (data) => {
+    console.log(`${data}`);
+    cliente.close();
+});
 
